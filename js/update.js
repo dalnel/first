38b4ec4be51ce45623109fe85e3d5109b419878e
dalnel <53857925+dalnel@users.noreply.github.com>
2020-10-31 12:03:39 +0800

    Add files via upload

---
+++ js/update.js
@@ -0,0 +1,13 @@
+var appUrl = https://script.google.com/macros/s/AKfycbxuMe5lhl08sPMU5P_SziBsdPDgx9LgaC5l1zzV45iVORoPXIU/exec,
+var parameter = {};
+$('#send').click(function()
+{
+    parameter={
+        name: $('#name').val(),
+        email: $('#email').val(),
+        message: $('#message').val(),
+    }
+    $.get(appUrl.val(), parameter);
+})
+  
+$.get(appUrl.val(), parameter);
