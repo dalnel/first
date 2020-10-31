362f456d43e21971949680909a01a346ab3629c0
dalnel <53857925+dalnel@users.noreply.github.com>
2020-10-31 13:17:20 +0800

    Update update.js

--- js/update.js
+++ js/update.js
@@ -1,4 +1,4 @@
-var appUrl = "https://script.google.com/macros/s/AKfycbxuMe5lhl08sPMU5P_SziBsdPDgx9LgaC5l1zzV45iVORoPXIU/exec",
+var appUrl = "https://script.google.com/macros/s/AKfycbxuMe5lhl08sPMU5P_SziBsdPDgx9LgaC5l1zzV45iVORoPXIU/exec";
 var parameter = {};
 $('#send').click(function()
 {
